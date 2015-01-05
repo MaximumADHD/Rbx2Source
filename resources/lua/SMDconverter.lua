5e2e044065d11cc70b469e1bc191f01d907acdaa
Max <MPG1019@aol.com>
2015-01-05 08:49:10 -0600

    Update SMDconverter.lua

--- resources/lua/SMDconverter.lua
+++ resources/lua/SMDconverter.lua
@@ -98,7 +98,6 @@ function calculateOrigin(obj,group)
 	for _,face in pairs(obj.Faces) do
 		if face.Group == group then
 			for _,coord in pairs(face.Coords) do
-				local vert = obj.Verts[coord.Vert]
 				for k,v in pairs(verts) do
 					table.insert(coords[k],v)
 				end
