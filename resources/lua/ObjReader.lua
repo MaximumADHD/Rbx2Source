3719837ac40fb7082e5bedbae6471abb787c59ca
Max <MPG1019@aol.com>
2015-02-05 11:50:11 -0600

    Optimizations and better tag explanations

--- resources/lua/ObjReader.lua
+++ resources/lua/ObjReader.lua
@@ -20,6 +20,35 @@ if not Vector3 then
 	require("Vector3")
 end
 
+function readTags(file)
+	local tags = {}
+	for line in mtlFile:gmatch("[^\r\n]+") do
+		if #line > 0 then
+			local process = ""
+			local readChars = 0
+			local block = { Items = {} }
+			for char in line:gmatch(".") do
+				readChars = readChars + 1
+				if char == " " then
+					if not block.Tag then
+						block.Tag = process
+					else
+						table.insert(block.Items,tonumber(process) or process)
+					end
+					process = ""
+				else
+					process = process .. char
+					if readChars == #line then
+						table.insert(block.Items,tonumber(process) or process)
+					end
+				end
+			end
+			table.insert(tags,block)
+		end
+	end
+	return tags
+end
+
 function parseOBJ(objFile,origin)
 	local origin = origin or Vector3.new()
 	local obj = {
@@ -30,70 +59,51 @@ function parseOBJ(objFile,origin)
 	}
 	local currentMtl = "";
 	local currentGroup = "root";
-	for line in objFile:gmatch("[^\r\n]+") do
-		if #line > 0 then
-			local info = {}
-			local tag = ""
-			local process = ""
-			local readChars = 0
-			for char in line:gmatch(".") do
-				readChars = readChars + 1
-				if char == " " then
-					if tag == "" then
-						tag = process
-					else
-						table.insert(info,tonumber(process) or process)
-					end
-					process = ""
+	local tags = readTags(objFile)
+	for _,v in pairs(tags) do
+		local tag = v.Tag
+		local info = v.Items
+		if tag == "usemtl" then -- Current Material in use
+			currentMtl = info[1]
+		elseif tag == "g" then
+			local group = info[1]
+			currentGroup = info[1]
+		elseif tag == "v" then -- A new Vert 
+			local vec = Vector3.new(unpack(info))
+			vec = (vec - origin) * meshScale
+			table.insert(obj.Verts,{vec.X,vec.Y,vec.Z})
+		elseif tag == "vn" then -- A new Normal
+			local vec = Vector3.new(unpack(info))
+			table.insert(obj.Norms,{vec.X,vec.Y,vec.Z})
+		elseif tag == "vt" then -- A new Texture Coordinate
+			table.insert(obj.Texs,info)
+		elseif tag == "f" then -- A face for the mesh
+			local face = {
+				Material = currentMtl;
+				Group = currentGroup;
+				Coords = {};
+			}
+			for _,pair in pairs(info) do
+				local triangle = {}
+				local v,t,n
+				-- The face definition format has a weird pattern format. Can't really explain this.
+				-- (%S+) is used to match an individual number using the Lua String Pattern algorithm stuff.
+				if type(pair) == "number" then 
+					v = tonumber(pair)
+				elseif string.find(pair,"//") then
+					v,n = string.match(pair,"(%S+)//(%S+)")
 				else
-					process = process .. char
-					if readChars == #line then
-						table.insert(info,tonumber(process) or process)
+					v,t,n = string.match(pair,"(%S+)/(%S+)/(%S+)")
+					if not v or not t or not n then
+						v,t = string.match(pair,"(%S+)/(%S+)")
 					end
 				end
+				triangle.Vert = tonumber(v)
+				triangle.Tex = tonumber(t)
+				triangle.Norm = tonumber(n)	
+				table.insert(face.Coords,triangle)
 			end
-			if tag == "usemtl" then
-				currentMtl = info[1]
-			elseif tag == "g" then
-				local group = info[1]
-				currentGroup = info[1]
-			elseif tag == "v" then
-				local vec = Vector3.new(unpack(info))
-				vec = (vec - origin) * meshScale
-				table.insert(obj.Verts,{vec.X,vec.Y,vec.Z})
-			elseif tag == "vn" then
-				local vec = Vector3.new(unpack(info))
-				table.insert(obj.Norms,{vec.X,vec.Y,vec.Z})
-			elseif tag == "vt" then
-				table.insert(obj.Texs,info)
-			elseif tag == "f" then
-				local face = {
-					Material = currentMtl;
-					Group = currentGroup;
-					Coords = {};
-				}
-				for _,pair in pairs(info) do
-					local triangle = {}
-					local v,t,n
-					-- The face definition format has a weird pattern format. Can't really explain this.
-					-- (%S+) is used to match an individual number using the Lua String Pattern algorithm stuff.
-					if type(pair) == "number" then 
-						v = tonumber(pair)
-					elseif string.find(pair,"//") then
-						v,n = string.match(pair,"(%S+)//(%S+)")
-					else
-						v,t,n = string.match(pair,"(%S+)/(%S+)/(%S+)")
-						if not v or not t or not n then
-							v,t = string.match(pair,"(%S+)/(%S+)")
-						end
-					end
-					triangle.Vert = tonumber(v)
-					triangle.Tex = tonumber(t)
-					triangle.Norm = tonumber(n)	
-					table.insert(face.Coords,triangle)
-				end
-				table.insert(obj.Faces,face)
-			end
+			table.insert(obj.Faces,face)
 		end
 	end
 	return obj
@@ -101,39 +111,20 @@ end
 
 function parseMTL(mtlFile)
 	local mtl = {}
+	local tags = readTags(mtlFile)
 	local currentMtl = ""
 	local dump
-	for line in mtlFile:gmatch("[^\r\n]+") do
-		if #line > 0 then
-			local info = {}
-			local tag = ""
-			local process = ""
-			local readChars = 0
-			for char in line:gmatch(".") do
-				readChars = readChars + 1
-				if char == " " then
-					if tag == "" then
-						tag = process
-					else
-						table.insert(info,process)
-					end
-					process = ""
-				else
-					process = process .. char
-					if readChars == #line then
-						table.insert(info,process)
-					end
-				end
+	for _,v in pairs(tags) do
+		local tag = v.Tag
+		local info = v.Items
+		if tag == "newmtl" then -- A new material
+			if dump then
+				table.insert(mtl,dump);
 			end
-			if tag == "newmtl" then
-				if dump then
-					table.insert(mtl,dump);
-				end
-				dump = {};
-				dump.Material = info[1];
-			elseif tag == "map_d" then
-				dump.HashTex = info[1];
-			end
+			dump = {};
+			dump.Material = info[1];
+		elseif tag == "map_d" then -- A texture map for the mesh
+			dump.HashTex = info[1];
 		end
 	end
 	if dump then
