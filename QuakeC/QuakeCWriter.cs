f9d6e011ea6e9034077af61ad19f5e5985a7e5a9
Max G <clonet1019@gmail.com>
2019-03-30 23:20:13 -0500

    Update QuakeCWriter.cs

--- QuakeC/QuakeCWriter.cs
+++ QuakeC/QuakeCWriter.cs
@@ -6,18 +6,18 @@ namespace Rbx2Source.QuakeC
     public class QuakeCWriter
     {
         private List<QuakeCItem> elements = new List<QuakeCItem>();
+        
+        public void Add(QuakeCItem item)
+        {
+            elements.Add(item);
+        }
 
         public QuakeCItem Add(string type, params object[] attributes)
         {
             QuakeCItem item = new QuakeCItem(type, attributes);
-            elements.Add(item);
+            Add(item);
 
             return item;
-        }
-
-        public void Add(QuakeCItem item)
-        {
-            elements.Add(item);
         }
 
         public override string ToString()
